e6b11c208a9ecb261a3863bff950866c980063dd
OM <olli.multanen@gmail.com>
2022-08-13 13:14:24 +0200

    2.13 maiden tiedot step2 done

--- maiden_tiedot/src/App.js
+++ maiden_tiedot/src/App.js
@@ -1,6 +1,12 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
+const Button = ({ handleClick, text, value}) => (
+  <button onClick={handleClick} value={value}>
+    {text}
+  </button>
+)
+
 const Filter = ({ filter, FilterChange }) => {
   return (
     <>
@@ -22,13 +28,15 @@ const DefaultMsg = () => {
   )
 }
 
-const CountryList = ({ search }) => {
+const CountryList = ({ search, handleClick}) => {
   return (
     <>
     {search && search.map((country) => {
       return (
         <div key={country.name.common}>
-          <li>{country.name.common}</li>
+          <li>{country.name.common}
+          <Button handleClick={handleClick} text="show" value={country.name.common}/>
+          </li>
         </div>
       )
     })}
@@ -61,15 +69,49 @@ const Results = ({search}) => {
   )
 }
 
-const Content = ({filterSize, query}) => {
-    if (filterSize.length >= 1 && query.length <= 10 && query.length !== 1) {
+const ButtonSelect = ({buttonSelect, handleClick}) => {
+  return (
+    <>
+      {buttonSelect && buttonSelect.map((country) => {
+        return (
+          <div key={country.name.common}>
+            <Button handleClick={handleClick} text="back" value=""/>
+            <h2>{country.name.common}</h2>
+            <p>capital {country.capital}</p>
+            <p>area {country.area}</p>
+            <p><b>languages:</b></p>
+            {Object.hasOwn(country, 'languages') && Object.entries(country.languages).map(([key, value]) => {
+            return (
+                <ul key={key}>
+                  <li>{value}</li>
+                </ul>
+            )
+        })}
+            <img src={country.flags.png} alt="country flag" width="180" height="180"></img>
+          </div>
+)
+})}
+    </>
+  )
+}
+
+const Content = ({filterSize, query, handleClick, buttonSelect, handleGoBack}) => {
+
+    if (filterSize.length >= 0 && query.length <= 10 && query.length !== 1 && buttonSelect.length !== 1) {
     return (
       <>
-      <CountryList search={query} />
+      <CountryList search={query} handleClick={handleClick}/>
+      </>
+    )
+  } else if (buttonSelect.length === 1) {
+    return (
+      <>
+      <ButtonSelect buttonSelect={buttonSelect} handleClick={handleGoBack}/>
       </>
     )
   }
-    else if (filterSize.length >= 1 && query.length === 1) {
+
+    else if (filterSize.length >= 0 && query.length === 1) {
       return (
         <Results search={query} />
       )
@@ -83,25 +125,33 @@ const Content = ({filterSize, query}) => {
 const App = () => {
 
   const [countries, setCountries] = useState([])
+  const [buttonSelect, SetButtonSelect] = useState([])
   const [filter, setFilter] = useState('')
 
   useEffect(() => {
     axios
         .get('https://restcountries.com/v3.1/all')
         .then(response => {
-
           setCountries(response.data)
         })
   })
   const handleFilterChange = (event) => {
     setFilter(event.target.value)
 }
+  const handleClick = (event) => {
+    console.log(event.target.value)
+    SetButtonSelect(countries.filter(country => country.name.common == event.target.value))
+}
+  const handleGoBack = () => {
+    SetButtonSelect("")
+  }
+
   const query = countries.filter(country => country.name.common.toLowerCase().includes(filter))
 
   return (
     <>
     < Filter filter={filter} FilterChange={handleFilterChange}/>
-    < Content filterSize={filter} query={query}/>
+    < Content filterSize={filter} query={query} handleClick={handleClick} buttonSelect={buttonSelect} handleGoBack={handleGoBack}/>
     </>
   );
 }
